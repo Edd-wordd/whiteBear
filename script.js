ca70862984b7253dc30cea7e67480b129e9c5270
Edd-wordd <edward.plasencio@gmail.com>
2020-05-13 12:54:36 -0700

    add valid classes

--- script.js
+++ script.js
@@ -28,7 +28,18 @@ $("#letsGoIndex").click(function () {
    var checkEmail1 = $("#email_1").val().length;
 
    if (checkEmail1 == 0) {
-      $("#email_1").addClass("alert alert-danger");
+      $("#email_1").addClass("is-invalid");
+      return false;
+   } else {
+      return true;
+   }
+});
+// // checks to see if password is input field is empty on sign up card
+$("#letsGoIndex").click(function () {
+   var checkPass1 = $("#password_1").val().length;
+
+   if (checkPass1 == 0) {
+      $("#password_1").addClass("is-invalid");
       return false;
    } else {
       return true;
@@ -39,53 +50,42 @@ $("#letsGoIndex").click(function () {
    var checkPass1 = $("#password_1").val().length;
 
    if (checkPass1 <= 8) {
-      $("#password_1").addClass("alert alert-danger");
-      return false;
-   } else {
-      return true;
-   }
-});
-// checks to see if password is input field is empty on sign up card
-$("#letsGoIndex").click(function () {
-   var checkPass1 = $("#password_1").val().length;
-
-   if (checkPass1 == 0) {
-      $("#password_1").addClass("alert alert-danger");
+      $("#password_1").addClass("passwordTooShort");
       return false;
    } else {
       return true;
    }
 });
 
-// checks to see if there only characters in the email input field when sign in button is clicked on login card
+// // checks to see if there only characters in the email input field when sign in button is clicked on login card
 $("#loginPage").click(function () {
    var checkEmail2 = $("#email_2").val().length;
 
    if (checkEmail2 == 0) {
-      $("#email_2").addClass("alert alert-danger");
+      $("#email_2").addClass("is-invalid");
       return false;
    } else {
       return true;
    }
 });
-// checks to see if there only characters in the password input field sign in button is clicked on login card
+// // checks to see if there only characters in the password input field sign in button is clicked on login card
 $("#loginPage").click(function () {
    var checkPass2 = $("#password_2").val().length;
 
    if (checkPass2 == 0) {
-      $("#password_2").addClass("alert alert-danger");
+      $("#password_2").addClass("is-invalid");
       return false;
    } else {
       return true;
    }
 });
 
-// checks to see if there only characters in the email input field when sign in button is clicked on login card
+// // checks to see if there only characters in the email input field when sign in button is clicked on login card
 $("#loginPage").click(function () {
    var checkPass2 = $("#password_2").val().length;
 
    if (checkPass2 <= 8) {
-      $("#password_2").addClass("alert alert-danger");
+      $("#password_2").addClass("is-invalid");
       return false;
    } else {
       return true;

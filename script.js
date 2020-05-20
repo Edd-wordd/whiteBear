e6126c99c13865c06ccf9d2c5b6ddd05e66f2fde
Edd-wordd <edward.plasencio@gmail.com>
2020-05-20 15:38:59 -0700

    cleaned up scripts.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(".car").click(function () {
 $("#save-imagery").click(function () {
    $("#success-banner").toggleClass("d-flex d-none");
 });
-
+// toggles the error alert
 $("#backToAnswer").click(function () {
    $("#error-banner").toggleClass("d-flex d-none");
 });
@@ -26,6 +26,8 @@ $("#letsGoIndex").click(function () {
 
    if (checkEmail1 == 0) {
       $("#email_1").addClass("is-invalid");
+   } else {
+      $("#email_1").addClass("is-valid");
    }
 });
 // // checks to see if password is input field is empty on sign up card
@@ -34,14 +36,10 @@ $("#letsGoIndex").click(function () {
 
    if (checkPass1 == 0) {
       $("#password_1").addClass("is-invalid");
-   }
-});
-// checks to see if password is at least 9 characters long on sign up card
-$("#letsGoIndex").click(function () {
-   var checkPass1 = $("#password_1").val().length;
-
-   if (checkPass1 <= 8) {
+   } else if (checkPass1 <= 8) {
       $("#password_1").addClass("passwordTooShort");
+   } else {
+      $("#password_1").addClass("is-valid");
    }
 });
 
@@ -51,9 +49,8 @@ $("#loginPage").click(function () {
 
    if (checkEmail2 == 0) {
       $("#email_2").addClass("is-invalid");
-      return false;
    } else {
-      return true;
+      $("#email_2").addClass("is-valid");
    }
 });
 // // checks to see if there only characters in the password input field sign in button is clicked on login card
@@ -62,21 +59,10 @@ $("#loginPage").click(function () {
 
    if (checkPass2 == 0) {
       $("#password_2").addClass("is-invalid");
-      return false;
-   } else {
-      return true;
-   }
-});
-
-// // checks to see if there only characters in the email input field when sign in button is clicked on login card
-$("#loginPage").click(function () {
-   var checkPass2 = $("#password_2").val().length;
-
-   if (checkPass2 <= 8) {
+   } else if (checkPass2 <= 8) {
       $("#password_2").addClass("is-invalid");
-      return false;
    } else {
-      return true;
+      $("#password_2").addClass("is-valid");
    }
 });
 
@@ -232,3 +218,369 @@ $("#save-imagery").click(function () {
    };
    console.log(imageryId);
 });
+
+// array with common passwords with 8 characters or more
+var largerThen8 = [];
+function scanList(arr) {
+   var newArr = arr.filter((obj) => {
+      obj.length > 8;
+      largerThen8.push(newArr[i]);
+   });
+   return largerThen8;
+}
+console.log(largerThen8);
+
+let commonPassword = [
+   "x002tp00",
+   "x4ww5qdr",
+   "x72jhhu3z",
+   "xaccess2",
+   "xakep1234",
+   "xboxlive",
+   "xcalibur",
+   "xcountry",
+   "xenocide",
+   "xenogear",
+   "xenophobia",
+   "xenophobic",
+   "xenophon",
+   "xexeylhf",
+   "xianzong",
+   "xiaoping",
+   "xiaoyua123",
+   "xinjiang",
+   "xohzi3g4",
+   "xpressmusic",
+   "xsvnd4b2",
+   "xsw21qaz",
+   "xsw23edc",
+   "xthtgfirf",
+   "xtutdfhf",
+   "xuanzang",
+   "xuanzong",
+   "xufrgemw",
+   "xxx12345",
+   "xxxp455w0rd5",
+   "xxxxxxx1",
+   "xyh28af4",
+   "xylophone",
+   "xzsawq21",
+   "y'knowwhati'msayin",
+   "y'understand",
+   "yachting",
+   "yaglasph",
+   "yakovlev",
+   "yakushova",
+   "yamagata",
+   "yamaha12",
+   "yamahar1",
+   "yamahar6",
+   "yamakasi",
+   "yamamoto",
+   "yamanashi",
+   "yamashita",
+   "yammering",
+   "yangzhou",
+   "yankeemp",
+   "yankees0",
+   "yankees1",
+   "yankees2",
+   "yankees3",
+   "yankees4",
+   "yankees7",
+   "yankees9",
+   "yankovic",
+   "yanochka",
+   "yanshi1982",
+   "yanukovych",
+   "yarborough",
+   "yarbrough",
+   "yardbird",
+   "yardbirds",
+   "yardstick",
+   "yarmouth",
+   "yaroslav",
+   "yaroslavl",
+   "yasacrac",
+   "yasuhiro",
+   "ybrbnbyf",
+   "ybrbnf_25",
+   "ybrbnjcbr",
+   "ybrjkftd",
+   "ybrjkftdbx",
+   "ybrjkftdyf",
+   "ycwvrxxh",
+   "yeahbaby",
+   "yeahrigh",
+   "yeahright",
+   "year2000",
+   "year2005",
+   "yearbook",
+   "yearbooks",
+   "yearning",
+   "yearnings",
+   "yekaterinburg",
+   "yelburton",
+   "yelena03",
+   "yellow12",
+   "yellow22",
+   "yellowhead",
+   "yellowish",
+   "yellowknife",
+   "yellowstone",
+   "yelverton",
+   "yemenite",
+   "yengeese",
+   "yeomanry",
+   "yes90125",
+   "yeshivas",
+   "yessenia",
+   "yesterda",
+   "yesterday",
+   "yesterdays",
+   "yfcnfcmz",
+   "yfcntymrf",
+   "yfcnz123",
+   "yfcnzvjz",
+   "yfcnzyfcnz",
+   "yfdbufnjh",
+   "yfeiybrb",
+   "yfgjktjy",
+   "yfhrjnbrb",
+   "yfltymrf",
+   "yfnfitymrf",
+   "yfxfkmybr",
+   "ygfxbkgt",
+   "yggdrasi",
+   "yielding",
+   "yildirim",
+   "yingling",
+   "yingyang",
+   "yitzchak",
+   "yjdbrjdf",
+   "yjdjcnbf",
+   "yjdsqgfhjkm",
+   "yjdsqujl",
+   "yjdujhjl",
+   "yjhbkmcr",
+   "ym3cautj",
+   "yogibear",
+   "yogyakarta",
+   "yohannan",
+   "yokohama",
+   "yokosuka",
+   "yokozuna",
+   "yorkshir",
+   "yorkshire",
+   "yorktown",
+   "yorkville",
+   "yosemite",
+   "yoshihiro",
+   "youandme",
+   "youlanda",
+   "youngblood",
+   "youngest",
+   "youngman",
+   "youngone",
+   "youngster",
+   "youngsters",
+   "youngstown",
+   "yourmama",
+   "yourmom1",
+   "yourname",
+   "yourself",
+   "yourselves",
+   "yousuck1",
+   "youthful",
+   "yoyodyne",
+   "ypsilanti",
+   "yqlgr667",
+   "yqmbevgk",
+   "yr8wdxcq",
+   "ytdxz2ca",
+   "ytngfhjkz",
+   "ytnhjufnm",
+   "ytrhjvfyn",
+   "ytyfdbcnm",
+   "yudhoyono",
+   "yugoslav",
+   "yugoslavia",
+   "yugoslavian",
+   "yuitre12",
+   "yujyd360",
+   "yuletide",
+   "yushchenko",
+   "yvelines",
+   "yvtte545",
+   "yxkck878",
+   "yy5rbfsc",
+   "yyyyyyy1",
+   "yzerman1",
+   "z1234567",
+   "z123456789",
+   "z123456z",
+   "z1x2c3v4",
+   "z1x2c3v4b5",
+   "z1x2c3v4b5n6m7",
+   "z1z2z3z4",
+   "z3cn2erv",
+   "zacatecas",
+   "zacharia",
+   "zachariah",
+   "zacharias",
+   "zachary1",
+   "zaharova",
+   "zakamatak",
+   "zalewski",
+   "zalgiris",
+   "zamarripa",
+   "zambales",
+   "zamboanga",
+   "zambrano",
+   "zamindar",
+   "zamoyski",
+   "zanesville",
+   "zantopia",
+   "zanzibar",
+   "zapatista",
+   "zaphod42",
+   "zaporizhia",
+   "zapruder",
+   "zaq!2wsx",
+   "zaq11qaz",
+   "zaq12345",
+   "zaq123wsx",
+   "zaq12qaz",
+   "zaq12wsx",
+   "zaq1xsw2",
+   "zaq1xsw2cde3",
+   "zaqwsx123",
+   "zaqwsxcde",
+   "zaqwsxcderfv",
+   "zaqxsw123",
+   "zaqxswcde",
+   "zaragoza",
+   "zaratustra",
+   "zarzuela",
+   "zasranec",
+   "zatunica",
+   "zawahiri",
+   "zaxscdvf",
+   "zbigniew",
+   "zcfvfzkexifz",
+   "zcxfcnkbdf",
+   "zcxfcnkbdfz",
+   "zealander",
+   "zealanders",
+   "zebra123",
+   "zechariah",
+   "zeebrugge",
+   "zeitgeist",
+   "zeitschrift",
+   "zeljeznicar",
+   "zemanova",
+   "zendejas",
+   "zenit2011",
+   "zeppelin",
+   "zeringue",
+   "zerocool",
+   "zesyrmvu",
+   "zhaozong",
+   "zhejiang",
+   "zhengzhou",
+   "zhjckfdf",
+   "zhongguo",
+   "zhongshan",
+   "zhongshu",
+   "zhongzong",
+   "zhytomyr",
+   "zidane10",
+   "ziegfeld",
+   "zielinski",
+   "zigazaga",
+   "ziggy123",
+   "zildjian",
+   "zillions",
+   "zimbabwe",
+   "zimbabwean",
+   "zimmerma",
+   "zimmerman",
+   "zimmermann",
+   "zinedine",
+   "zinfandel",
+   "zinoviev",
+   "zionists",
+   "zipdrive",
+   "zippy123",
+   "zirconia",
+   "zirconium",
+   "zjses9evpa",
+   "zldej102",
+   "zoidberg",
+   "zolushka",
+   "zombie13",
+   "zookeeper",
+   "zoological",
+   "zoologist",
+   "zooplankton",
+   "zootsuit",
+   "zoroaster",
+   "zoroastrian",
+   "zoroastrianism",
+   "zoroastrians",
+   "zorro123",
+   "zpflhjn1",
+   "zqjphsyf6ctifgu",
+   "zrjdktdf",
+   "zse45rdx",
+   "zse4xdr5",
+   "zsecyus56",
+   "zucchero",
+   "zucchini",
+   "zuckerberg",
+   "zuckerman",
+   "zukowski",
+   "zulfikar",
+   "zulfiqar",
+   "zvonareva",
+   "zweibrucken",
+   "zx123456",
+   "zx123456789",
+   "zxasqw12",
+   "zxc12345",
+   "zxc123456",
+   "zxc123zxc",
+   "zxcasd123",
+   "zxcasdqw",
+   "zxcasdqwe",
+   "zxcasdqwe123",
+   "zxcqweasd",
+   "zxcv1234",
+   "zxcv4321",
+   "zxcvasdf",
+   "zxcvb123",
+   "zxcvb1234",
+   "zxcvb12345",
+   "zxcvbasdfg",
+   "zxcvbn12",
+   "zxcvbn123",
+   "zxcvbn123456",
+   "zxcvbnm.",
+   "zxcvbnm1",
+   "zxcvbnm12",
+   "zxcvbnm123",
+   "zxcvbnm123456789",
+   "zxcvbnma",
+   "zxcvbnmm",
+   "zxcvbnmz",
+   "zxcvfdsa",
+   "zxcvvcxz",
+   "zz123456",
+   "zz8807zpl",
+   "zzxxccvv",
+   "zzzxxxccc",
+   "zzzzxxxx",
+   "zzzzzzz1",
+];
+scanList(commonPassword);
+console.log(largerThen8);
